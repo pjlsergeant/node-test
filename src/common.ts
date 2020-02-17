48348c45d82472510571f3d3edf4d5cbf41e0691
Michael Storgaard <git@storgaard.io>
2020-02-17 13:15:42 +0100

    Added TypedSinonStub and exported common

--- src/common.ts
+++ src/common.ts
@@ -2,12 +2,17 @@ import crypto from 'crypto'
 import fs from 'fs'
 import os from 'os'
 import path from 'path'
+import sinon from 'sinon'
 import { promisify } from 'util'
 
 const mkdirAsync = promisify(fs.mkdir)
 
 // https://github.com/microsoft/TypeScript/issues/1897
 export type Json = null | boolean | number | string | Json[] | { [prop: string]: Json }
+
+// Usage: let stub: TypedSinonStub<typeof fs.readFile>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type TypedSinonStub<A extends (...args: any) => any> = sinon.SinonStub<Parameters<A>, ReturnType<A>>
 
 export async function createTempDirectory(): Promise<string> {
   const tmpDir = os.tmpdir() + path.sep + crypto.randomBytes(8).toString('hex')
