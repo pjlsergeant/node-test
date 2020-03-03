0a6995f7075d49487935180176138eb844eea601
Troels Liebe Bentsen <tlb@nversion.dk>
2020-03-03 11:46:10 +0100

    Implement mysql config handling

---
+++ src/mysql/common.test.ts
@@ -0,0 +1,62 @@
+import { CommandEmulation } from '../command-emulation'
+import { generateMySQLServerConfig, getMySQLServerConfig, getMySQLServerVersionString } from './common'
+import { mysqlHelpVerbose } from './resources/mysqld-help-verbose.text'
+
+describe('generateMySQLServerConfig', () => {
+  it('should generate generic config', async () => {
+    const config = generateMySQLServerConfig('/tmp/test')
+    expect(config).toMatchSnapshot()
+  })
+
+  it('should set user to mysql', async () => {
+    const config = generateMySQLServerConfig('/tmp/test', { mysqld: { user: 'mysql' } })
+    expect(config).toMatchSnapshot()
+  })
+})
+
+describe('getMySQLServerVersionString', () => {
+  const commandEmulation = new CommandEmulation()
+
+  afterAll(async () => {
+    //await commandEmulation.cleanup()
+  })
+
+  it('should return the version string', async () => {
+    await commandEmulation.registerCommand('mysqld', () => {
+      console.log('/usr/local/Cellar/mysql/8.0.17_1/bin/mysqld  Ver 8.0.17 for osx10.14 on x86_64 (Homebrew)')
+    })
+    const versionStringPromise = getMySQLServerVersionString('mysqld')
+    await expect(versionStringPromise).resolves.toEqual(
+      '/usr/local/Cellar/mysql/8.0.17_1/bin/mysqld  Ver 8.0.17 for osx10.14 on x86_64 (Homebrew)'
+    )
+  })
+
+  it('should return an error', async () => {
+    await commandEmulation.registerCommand('mysqld', () => {
+      process.exit(1)
+    })
+    const versionStringPromise = getMySQLServerVersionString('mysqld')
+    await expect(versionStringPromise).rejects.toEqual(new Error('mysqld --version returned non 0 exit code:\n'))
+  })
+})
+
+describe('getMySQLServerConfig', () => {
+  const commandEmulation = new CommandEmulation()
+
+  afterAll(async () => {
+    await commandEmulation.cleanup()
+  })
+
+  it('should return the current mysql config', async () => {
+    await commandEmulation.registerCommand(
+      'mysqld',
+      data => {
+        console.log(data)
+      },
+      null,
+      mysqlHelpVerbose
+    )
+    const mysqldConfigPromise = getMySQLServerConfig('mysqld', '/tmp/test')
+    await expect(mysqldConfigPromise).resolves.toMatchSnapshot()
+  })
+})
---
+++ src/mysql/common.ts
@@ -0,0 +1,108 @@
+import fs from 'fs'
+import os from 'os'
+
+import { RunProcess } from './run-process'
+
+export interface MySQLServerConfig {
+  [key: string]: { [key: string]: string }
+}
+
+export async function getMySQLServerVersionString(mysqldPath: string): Promise<string> {
+  const cmd = new RunProcess(mysqldPath, ['--version'])
+  const outputData: Buffer[] = []
+  cmd.stdout?.on('data', chunk => {
+    outputData.push(chunk)
+  })
+  cmd.stderr?.on('data', chunk => {
+    outputData.push(chunk)
+  })
+  const code = await new Promise<number>(resolve => cmd.on('exit', code => resolve(code || 0)))
+  const stdout = Buffer.concat(outputData).toString('utf8')
+  if (code !== 0) {
+    throw new Error(`${mysqldPath} --version returned non 0 exit code:\n${stdout}`)
+  }
+  return stdout.replace(/\r?\n$/, '')
+}
+
+export async function getMySQLServerConfig(mysqldPath: string, mysqlBaseDir: string): Promise<MySQLServerConfig> {
+  const cmd = new RunProcess(mysqldPath, [`--defaults-file=${mysqlBaseDir}/my.cnf`, '--help', '--verbose'])
+  const outputData: Buffer[] = []
+  cmd.stdout?.on('data', chunk => {
+    outputData.push(chunk)
+  })
+  cmd.stderr?.on('data', chunk => {
+    outputData.push(chunk)
+  })
+  const code = await new Promise<number>(resolve => cmd.on('exit', code => resolve(code || 0)))
+  const output = Buffer.concat(outputData).toString('utf8')
+  if (code !== 0) {
+    throw new Error(`Failed to dump configuration(${mysqldPath} --help --verbose): \n${output}\n`)
+  }
+
+  const variables: { [key: string]: string } = {}
+  const variablesSection = output.toString().match(/Variables.+?Value.+?-{50,}\s+-{10}.+?\n(.+?)\n\n/s)
+  if (variablesSection != null) {
+    for (const variableLine of variablesSection[1].split('\n')) {
+      const variableMatch = variableLine.match(/^([^\s]+)\s+(.+?)$/)
+      if (variableMatch) {
+        variables[variableMatch[1]] = variableMatch[2]
+      }
+    }
+  }
+  return { mysqld: variables }
+}
+
+export function generateMySQLServerConfig(mysqlBaseDir: string, myCnfCustom: MySQLServerConfig = {}): string {
+  const myCnf: MySQLServerConfig = {
+    // Defaults
+    mysqld: {
+      'bind-address': '127.0.0.1',
+      'pid-file': `${mysqlBaseDir}/data/mysqld.local.pid`,
+      socket: `${mysqlBaseDir}/mysql.sock`,
+      datadir: `${mysqlBaseDir}/data`,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      secure_file_priv: `${mysqlBaseDir}/files`,
+      tmpdir: os.tmpdir()
+    },
+    'mysqld-8.0': {
+      mysqlx: '0'
+    }
+  }
+
+  // Merge configs
+  for (const section of Object.keys(myCnfCustom)) {
+    for (const key of Object.keys(myCnfCustom[section])) {
+      myCnf[section] = myCnf[section] || {}
+      myCnf[section][key] = myCnfCustom[section][key]
+    }
+  }
+
+  const myCnfLines: string[] = []
+  for (const section of Object.keys(myCnf)) {
+    myCnfLines.push(`[${section}]`)
+    for (const key of Object.keys(myCnf[section])) {
+      myCnfLines.push(`${key}=${myCnf[section][key]}`)
+    }
+  }
+
+  return myCnfLines.join(`\n`) + '\n'
+}
+
+export function isPidFileRunning(pidFile: string): boolean | void {
+  if (!fs.existsSync(pidFile)) {
+    return false
+  }
+  const pidStr = fs
+    .readFileSync(pidFile)
+    .toString()
+    .replace(/\s+$/s, '')
+  if (!pidStr.match(/^\d+$/)) {
+    return false
+  }
+  const pid = parseInt(pidStr, 10)
+  try {
+    return process.kill(pid, 0)
+  } catch (e) {
+    return e.code === 'EPERM'
+  }
+}
