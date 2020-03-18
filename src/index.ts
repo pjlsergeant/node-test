b5a5999a86c795c187be930640cf519ee73f2a67
Viterkim <vkc@connectedcars.dk>
2020-03-18 15:05:56 +0100

    Fix linting

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,26 @@ export { Json, TypedSinonStub } from './common'
 export { HttpServer, HttpServerOptions } from './http/http-server'
 export { HttpsServer, HttpsServerOptions } from './http/https-server'
 export { WebServer, WebServerOptions } from './http/web-server'
-export { readHttpMessageBody, HttpIncomingMessage, HttpJsonRequest, HttpRequest, HttpRequestListener, HttpServerError, HttpTextRequest } from './http/http-common'
-export { CommandEmulation, CommandEmulationOptions, isPidFileRunning, isPidRunning, createTempDirectory, ExitBeforeOutputMatchError, ExitInformation, RunProcess, StopBecauseOfOutputError, TimeoutError, readPidFile, touchFiles } from './unix'
+export {
+  readHttpMessageBody,
+  HttpIncomingMessage,
+  HttpJsonRequest,
+  HttpRequest,
+  HttpRequestListener,
+  HttpServerError,
+  HttpTextRequest
+} from './http/http-common'
+export {
+  CommandEmulation,
+  CommandEmulationOptions,
+  isPidFileRunning,
+  isPidRunning,
+  createTempDirectory,
+  ExitBeforeOutputMatchError,
+  ExitInformation,
+  RunProcess,
+  StopBecauseOfOutputError,
+  TimeoutError,
+  readPidFile,
+  touchFiles
+} from './unix'
