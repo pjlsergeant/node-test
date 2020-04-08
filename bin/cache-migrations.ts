d98989f605bd909c372707d3630adcebb29f6b99
Troels Liebe Bentsen <tlb@nversion.dk>
2020-04-08 17:20:38 +0200

    Add more test timing

--- bin/cache-migrations.ts
+++ bin/cache-migrations.ts
@@ -21,28 +21,32 @@ async function main(argv: string[]): Promise<number> {
     const mySqlServer = new MySQLServer({ mysqlBaseDir: tmpDir, ignoreCache: true })
     cleanup.push(mySqlServer)
     const [mysqlServerTiming] = await time(mySqlServer.waitForStarted())
-    console.log(`mysqld no cache start: ${mysqlServerTiming / 1000}`)
+    console.log(`mysqld no cache start: ${mysqlServerTiming / 1000}ms`)
+
+    const testResumeMySqlServer = new MySQLServer({ mysqlBaseDir: tmpDir, ignoreCache: true })
+    const [testResumeMysqlServerTiming] = await time(testResumeMySqlServer.waitForStarted())
+    console.log(`mysqld resume start: ${testResumeMysqlServerTiming / 1000}ms`)
 
     const mySqlClient = new MySQLClient({ port: await mySqlServer.getListenPort() })
     const migrate = new Migrate({ mysqlClient: mySqlClient, migrationsDir: 'data/migrations', ignoreCache: true })
     const [migrationTiming] = await time(migrate.migrate())
-    console.log(`migration: ${migrationTiming / 1000}`)
+    console.log(`migration: ${migrationTiming / 1000}ms`)
 
     const [cacheTiming] = await time(migrate.cacheSchemas())
-    console.log(`schema cache: ${cacheTiming / 1000}`)
+    console.log(`schema cache: ${cacheTiming / 1000}ms`)
 
     const [snapshotTiming] = await time(mySqlServer.saveAsCustomInitState())
-    console.log(`custom init snapshot: ${snapshotTiming / 1000}`)
+    console.log(`custom init snapshot: ${snapshotTiming / 1000}ms`)
 
     const testCleanMySqlServer = new MySQLServer({ ignoreCustomCache: true })
     cleanup.push(testCleanMySqlServer)
     const [testCleanMysqlServerTiming] = await time(testCleanMySqlServer.waitForStarted())
-    console.log(`mysqld clean start: ${testCleanMysqlServerTiming / 1000}`)
+    console.log(`mysqld clean start: ${testCleanMysqlServerTiming / 1000}ms`)
 
     const testCustomMySqlServer = new MySQLServer()
     cleanup.push(testCustomMySqlServer)
     const [testCustomMysqlServerTiming] = await time(testCustomMySqlServer.waitForStarted())
-    console.log(`mysqld custom start: ${testCustomMysqlServerTiming / 1000}`)
+    console.log(`mysqld custom start: ${testCustomMysqlServerTiming / 1000}ms`)
   } finally {
     for (const server of cleanup) {
       await server.kill()
